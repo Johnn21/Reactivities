8abf8919ec67f87cfba8b3a7eebd0fababd92e85
Johnn21 <cata_xdr21@yahoo.com>
2022-12-15 15:11:28 +0200

    End of section 10

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,9 +1,12 @@
-import { RouteObject } from "react-router";
+import { Navigate, RouteObject } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import HomePage from "../../features/activities/home/HomePage";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
+import TestErrors from "../../features/errors/TestError";
 import App from "../layout/App";
 
 export const routes: RouteObject[] = [
@@ -11,10 +14,14 @@ export const routes: RouteObject[] = [
         path: '/',
         element: <App />,
         children: [
-            {path: 'activities', element: <ActivityDashboard />},
-            {path: 'activities/:id', element: <ActivityDetails />},
-            {path: 'createActivity', element: <ActivityForm key='create'/>},
-            {path: 'manage/:id', element: <ActivityForm key='manage'/>}
+            { path: 'activities', element: <ActivityDashboard /> },
+            { path: 'activities/:id', element: <ActivityDetails /> },
+            { path: 'createActivity', element: <ActivityForm key='create' /> },
+            { path: 'manage/:id', element: <ActivityForm key='manage' /> },
+            { path: 'errors', element: <TestErrors /> },
+            { path: 'not-found', element: <NotFound /> },
+            { path: 'server-error', element: <ServerError /> },
+            { path: '*', element: <Navigate replace to='/not-found' /> }
         ]
     }
 ]
